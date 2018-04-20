0a6dbf440fdf3c75d32b264e64aca31b2f95a8ee
Kieran Wood <kieranw098@gmail.com>
2018-04-20 19:08:18 +0000

    Updated text version to test catholic angels

--- python/src/textversion.py
+++ python/src/textversion.py
@@ -1,8 +1,11 @@
 from greekgods import *
 from hindugods import *
+from catholicangels import *
 
 if __name__ == '__main__':
     zuess = zuess()
     print(zuess)
     vishnu = vishnu()
     print(vishnu)
+    seraph = seraph("Seraph", 1000000, 1000000, 1000, 1000)
+    print(seraph)
